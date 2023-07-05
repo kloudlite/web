74c3c0aeab3295628e15480c55c179886d28e716
Karthik Thirumalasetti <karthik@kloudlite.io>
2023-07-05 13:42:48 +0530

    documentation wip

---
+++ src/apps/devdoc/root.jsx
@@ -0,0 +1,11 @@
+import root, {links as baseLinks} from "~/lib/app-setup/root.jsx"
+import authStylesUrl from "./styles/index.css";
+
+export const links = ()=>{
+    return [
+        ...baseLinks(),
+        {rel: "stylesheet", href: authStylesUrl}
+    ]
+}
+
+export default root;
