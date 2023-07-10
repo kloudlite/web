ef744e1221c9ea84e04c3541b731424e2f93d651
Bikash Ojha <bikashojha@Karthiks-iMac.local>
2023-07-10 16:56:32 +0530

    wip

--- src/design-system/components/atoms/chip.jsx
+++ src/design-system/components/atoms/chip.jsx
@@ -1,23 +1,16 @@
 import PropTypes from 'prop-types';
 import { XFill } from '@jengaicons/react';
 import { cn } from '../utils';
+import * as RovingFocusGroup from '@radix-ui/react-roving-focus';
+import { cloneElement, forwardRef, useRef, useState } from 'react';
 
-export const ChipTypes = Object.freeze({
+const ChipTypes = Object.freeze({
     BASIC: "BASIC",
     REMOVABLE: "REMOVABLE",
     CLICKABLE: "CLICKABLE"
 })
 
-
-export const Chip = ({ label, disabled, type = ChipTypes.BASIC, onClose, prefix, onClick }) => {
-
-    let Component = "div"
-    if (type === ChipTypes.CLICKABLE) {
-        Component = "button"
-    }
-
-    let Prefix = prefix
-
+const ChipBase = forwardRef(({ id, label, disabled, type = ChipTypes.BASIC, onClose, Prefix, onClick, Component, active = false, ...props }, ref) => {
     return (
         <Component
             className={cn(
@@ -47,6 +40,8 @@ export const Chip = ({ label, disabled, type = ChipTypes.BASIC, onClose, prefix,
                 }
             )}
             onClick={onClick}
+            {...props}
+            ref={ref}
         >
             {
                 Prefix && type != ChipTypes.CLICKABLE && ((typeof Prefix == "string") ? <span className='bodySm text-text-soft'>{Prefix}</span> : <Prefix size={16} color="currentColor" />)
@@ -55,24 +50,86 @@ export const Chip = ({ label, disabled, type = ChipTypes.BASIC, onClose, prefix,
                 {label}
             </span>
             {
-                type == ChipTypes.REMOVABLE && <button
-                    disabled={disabled}
-                    onClick={onClose}
-                    className={cn('outline-none flex items-center rounded-sm ring-offset-0 justify-center hover:bg-surface-hovered active:bg-surface-pressed',
-                        {
-                            "cursor-default": disabled
-                        })}>
-                    <XFill size={16} color="currentColor" />
-                </button>
+                type == ChipTypes.REMOVABLE &&
+                <RovingFocusGroup.Item asChild focusable active={active}>
+                    <button
+                        disabled={disabled}
+                        onClick={() => {
+                            if (onClose)
+                                onClose(id)
+                        }}
+                        onKeyDown={(e) => {
+                            if (e.key === "Backspace" || e.key === "Delete") {
+                                if (onClose)
+                                    onClose(id)
+                            }
+                        }}
+                        className={cn('outline-none flex items-center rounded-sm ring-offset-0 justify-center hover:bg-surface-hovered active:bg-surface-pressed',
+                            {
+                                "cursor-default": disabled
+                            })}>
+                        <XFill size={16} color="currentColor" />
+                    </button>
+                </RovingFocusGroup.Item>
             }
         </Component>
     );
+})
+
+export const Chip = ({ id, label, disabled, type = ChipTypes.BASIC, prefix, onClick, onClose, active = false }) => {
+
+    let Component = "div"
+    if (type === ChipTypes.CLICKABLE) {
+        Component = "button"
+    }
+    if (type == ChipTypes.CLICKABLE)
+        return <RovingFocusGroup.Item asChild focusable active={active}>
+            <ChipBase id={id} label={label} disabled={disabled} type={type} Prefix={prefix} Component={Component} onClick={onClick} onClose={onClose} active={active} />
+        </RovingFocusGroup.Item>
+    return <ChipBase id={id} label={label} disabled={disabled} type={type} Prefix={prefix} Component={Component} onClick={onClick} onClose={onClose} active={active} />
 };
 
 
+const ChipGroup = ({ onClick, onRemove, children }) => {
+    let ref = useRef(null)
+    const [active, setActive] = useState(false)
+    return <RovingFocusGroup.Root loop ref={ref} onEntryFocus={(e) => {
+        setActive(true)
+    }} >
+        <div className={cn("flex flex-row gap-2")}>
+            {Array.isArray(children)
+                ?
+                children.map((child, index) => cloneElement(child, {
+                    onClick: onClick,
+                    active: active && index == 0,
+                    onClose: (e) => {
+                        if (onRemove) {
+                            onRemove(e)
+                            ref.current.focus()
+                        }
+                    },
+                    key: `chip-group-item-${index}`
+                }))
+                :
+                cloneElement(children, {
+                    onClick: onClick,
+                    onClose: (e) => {
+                        if (onRemove) {
+                            onRemove(e)
+                            ref.current.focus()
+                        }
+                    },
+                })}
+        </div>
+    </RovingFocusGroup.Root>
+}
 
+ChipGroup.Chip = Chip
+ChipGroup.ChipType = ChipTypes
+export default ChipGroup
 
 Chip.propTypes = {
+    id: PropTypes.any.isRequired,
     label: PropTypes.string.isRequired,
     disabled: PropTypes.bool,
     onClose: PropTypes.func,
@@ -86,3 +143,15 @@ Chip.defaultProps = {
     onClose: null,
     disabled: false,
 };
+
+ChipGroup.propTypes = {
+    onClick: PropTypes.func,
+    onRemove: PropTypes.func,
+}
+
+ChipGroup.defaultProps = {
+    children: <>
+        <ChipGroup.Chip id={0} label='hello' />
+        <ChipGroup.Chip id={1} label='hi' type={ChipGroup.ChipType.REMOVABLE} />
+    </>
+}
