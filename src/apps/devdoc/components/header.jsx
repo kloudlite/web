06f78540c9d2c8d34005a9d577c4e754fe255338
Bikash Ojha <bikashojha@Karthiks-iMac.local>
2023-07-12 18:40:56 +0530

    refactor

--- src/apps/devdoc/components/header.jsx
+++ src/apps/devdoc/components/header.jsx
@@ -1,8 +1,8 @@
-import {BrandLogo} from "~/components/branding/brand-logo.jsx";
-import {Link} from "@remix-run/react";
-import {TextInput} from "~/components/atoms/input.jsx";
-import {Search} from "@jengaicons/react";
-import {Chip} from "~/components/atoms/chip.jsx";
+import { BrandLogo } from "~/components/branding/brand-logo.jsx";
+import { Link } from "@remix-run/react";
+import { TextInput } from "~/components/atoms/input.jsx";
+import { Search } from "@jengaicons/react";
+import { Chip } from "~/components/atoms/chip-group.jsx";
 
 export const Header = () => {
     return <div className={"border-b sticky h-docHeaderHeight top-0 z-40 backdrop-blur bg-white/80"}>
@@ -14,7 +14,7 @@ export const Header = () => {
                 <Link prefetch to={"/help"}>Help</Link>
             </div>
             <div className={"flex flex-row gap-4 p-4 items-center"}>
-                <TextInput placeholder={"Search"} prefixIcon={Search} suffix={<Chip label={"C+K"} />}  />
+                <TextInput placeholder={"Search"} prefixIcon={Search} suffix={<Chip label={"C+K"} />} />
                 <Link prefetch to={"https://google.com"}>Feedback</Link>
                 <Link to={"https://google.com"}>Login</Link>
                 <Link to={"https://google.com"}>Signup</Link>
