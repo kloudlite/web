fe87c29358e85846e9dac2e854ba1cdd33a0d476
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-07-13 17:52:31 +0530

    :art: Eslint setup and little formating
    eslint setupt was not done, now eslint working and formated some js-files according to eslint rule.

--- src/design-system/components/atoms/button.jsx
+++ src/design-system/components/atoms/button.jsx
@@ -1,7 +1,7 @@
 import { forwardRef } from 'react';
 import PropTypes from 'prop-types';
-import { DefaultLinkComp } from './_link';
 import { motion } from 'framer-motion';
+import { DefaultLinkComp } from './_link';
 import { cn } from '../utils';
 
 export const ButtonStyles = [
@@ -17,186 +17,303 @@ export const ButtonStyles = [
   'primary-plain',
   'secondary-plain',
   'critical-plain',
-]
-export const IconButtonStyles = [
-  'outline',
-  'basic',
-  'plain'
-]
+];
+export const IconButtonStyles = ['outline', 'basic', 'plain'];
 
-export const AriaButton = "button"
+export const AriaButton = 'button';
 
-export const ButtonBase = forwardRef(({
-  variant,
-  onClick,
-  href,
-  content,
-  type,
-  disabled,
-  sharpLeft = false,
-  sharpRight = false,
-  noBorder,
-  className,
-  noRounded,
-  noRing,
-  prefix,
-  iconOnly = false,
-  suffix,
-  selected = false,
-  block = false,
-  LinkComponent = DefaultLinkComp,
-  ...props
-}, ref) => {
+export const ButtonBase = forwardRef(
+  (
+    {
+      variant,
+      onClick,
+      href,
+      content,
+      type,
+      disabled,
+      sharpLeft = false,
+      sharpRight = false,
+      noBorder,
+      className,
+      noRounded,
+      noRing,
+      prefix,
+      iconOnly = false,
+      suffix,
+      selected = false,
+      block = false,
+      LinkComponent = DefaultLinkComp,
+      ...props
+    },
+    ref
+  ) => {
+    let Component = motion.button;
+    const extraProps = {};
 
-  let Component = motion.button
-  let extraProps = {}
+    extraProps.onClick = onClick;
 
-  extraProps.onClick = onClick
+    if (href) {
+      Component = motion(LinkComponent);
+      extraProps.to = href;
+    } else {
+      extraProps.disabled = disabled;
+    }
 
-  if (href) {
-    Component = motion(LinkComponent)
-    extraProps.to = href
-  } else {
-    extraProps.disabled = disabled
+    const Suffix = suffix;
+    const Prefix = prefix;
+
+    return (
+      <Component
+        initial={{ scale: 1 }}
+        whileTap={{ scale: 0.99 }}
+        ref={ref}
+        type={type}
+        className={cn(
+          {
+            'w-full': block,
+            'w-fit': !block,
+          },
+          {
+            'bodyMd-medium':
+              variant !== 'primary-plain' &&
+              variant !== 'secondary-plain' &&
+              variant !== 'critical-plain' &&
+              variant !== 'plain',
+            bodyMd:
+              variant === 'primary-plain' ||
+              variant === 'secondary-plain' ||
+              variant !== 'critical-plain' ||
+              variant !== 'plain',
+          },
+          'relative ring-offset-1',
+          'outline-none shadow-button',
+          'flex flex-row gap-lg items-center justify-center',
+          'disabled:text-text-disabled',
+          {
+            ...(noRing
+              ? {}
+              : {
+                'focus-visible:ring-2 focus:ring-border-focus focus:z-10': true,
+              }),
+          },
+          {
+            ...(noRounded
+              ? {}
+              : {
+                'rounded-none': sharpLeft && sharpRight,
+                'rounded-r': sharpLeft && !sharpRight,
+                'rounded-l': !sharpLeft && sharpRight,
+                rounded: !sharpLeft && !sharpRight,
+              }),
+          },
+          'transition-all',
+          'disabled:pointer-events-none',
+          {
+            ...(noBorder
+              ? { 'border-none': true }
+              : {
+                'border-border-default disabled:border-border-disabled':
+                  variant === 'basic' || variant === 'outline',
+                'border-border-primary disabled:border-border-disabled':
+                  variant === 'primary' || variant === 'primary-outline',
+                'border-border-secondary disabled:border-border-disabled':
+                  variant === 'secondary' || variant === 'secondary-outline',
+                'border-border-critical disabled:border-border-disabled':
+                  variant === 'critical-outline' || variant === 'critical',
+                'border-none':
+                  variant === 'plain' ||
+                  variant === 'primary-plain' ||
+                  variant === 'critical-plain' ||
+                  variant === 'secondary-plain',
+                border: !(
+                  variant === 'plain' ||
+                  variant === 'primary-plain' ||
+                  variant === 'critical-plain' ||
+                  variant === 'secondary-plain'
+                ),
+              }),
+          },
+          {
+            'bg-surface-basic-default hover:bg-surface-basic-hovered active:bg-surface-basic-pressed disabled:bg-surface-basic-default':
+              variant === 'basic',
+            'bg-surface-basic-pressed hover:bg-surface-basic-pressed active:bg-surface-basic-pressed disabled:bg-surface-basic-default':
+              variant === 'basic' && selected,
+            'bg-surface-primary-default hover:bg-surface-primary-hovered active:bg-surface-primary-pressed disabled:bg-surface-basic-default':
+              variant === 'primary',
+            'bg-surface-secondary-default hover:bg-surface-secondary-hovered active:bg-surface-secondary-pressed disabled:bg-surface-basic-default':
+              variant === 'secondary',
+            'bg-surface-critical-default hover:bg-surface-critical-hovered active:bg-surface-critical-pressed disabled:bg-surface-basic-default':
+              variant === 'critical',
+            'bg-none hover:bg-surface-critical-subdued active:bg-surface-critical-subdued':
+              variant === 'critical-outline',
+            'bg-none hover:bg-surface-primary-subdued active:bg-surface-primary-subdued':
+              variant === 'primary-outline',
+            'bg-none hover:bg-surface-secondary-subdued active:bg-surface-secondary-subdued':
+              variant === 'secondary-outline',
+            'bg-none hover:bg-surface-basic-hovered active:bg-surface-basic-pressed':
+              variant === 'outline',
+            'bg-surface-basic-pressed shadow-none hover:bg-surface-basic-hovered active:bg-surface-basic-pressed hover:shadow-button active:shadow-button':
+              variant === 'outline' && selected,
+            'bg-none shadow-none active:bg-surface-basic-pressed active:shadow-button':
+              variant === 'plain' && !iconOnly,
+            'bg-none shadow-none active:bg-surface-primary-pressed active:shadow-button':
+              variant === 'primary-plain',
+            'bg-none shadow-none active:bg-surface-secondary-pressed active:shadow-button':
+              variant === 'secondary-plain',
+            'bg-none shadow-none active:bg-surface-critical-pressed active:shadow-button':
+              variant === 'critical-plain',
+            'bg-none shadow-none hover:bg-surface-basic-hovered active:bg-surface-basic-pressed active:shadow-button':
+              variant === 'plain' && iconOnly,
+          },
+          {
+            'text-text-default':
+              variant === 'basic' ||
+              variant === 'plain' ||
+              variant === 'outline',
+            'active:text-text-on-primary':
+              variant === 'primary-plain' ||
+              variant === 'critical-plain' ||
+              variant === 'secondary-plain',
+            'text-text-on-primary':
+              variant === 'primary' ||
+              variant === 'critical' ||
+              variant === 'secondary' ||
+              variant === 'secondary-outline',
+            'text-text-critical hover':
+              variant === 'critical-outline' || variant === 'critical-plain',
+            'text-text-primary':
+              variant === 'primary-outline' || variant === 'primary-plain',
+            'text-text-secondary':
+              variant === 'secondary-plain' || variant === 'secondary-outline',
+          },
+          {
+            'focus:underline': noRing,
+          },
+          {
+            'hover:underline':
+              variant === 'plain' ||
+              variant === 'primary-plain' ||
+              variant === 'critical-plain' ||
+              variant === 'secondary-plain',
+          },
+          {
+            'px-2xl py-lg':
+              !iconOnly &&
+              !(
+                variant === 'plain' ||
+                variant === 'primary-plain' ||
+                variant === 'critical-plain' ||
+                variant === 'secondary-plain'
+              ),
+            'px-md py-sm':
+              !iconOnly &&
+              (variant === 'plain' ||
+                variant === 'primary-plain' ||
+                variant === 'critical-plain' ||
+                variant === 'secondary-plain'),
+            'p-lg': iconOnly,
+          },
+          className
+        )}
+        {...props}
+        {...extraProps}
+      >
+        {prefix && <Prefix size={iconOnly ? 20 : 16} color="currentColor" />}
+        {!iconOnly && content}
+        {suffix && !iconOnly && <Suffix size={16} color="currentColor" />}
+      </Component>
+    );
   }
+);
 
-  let Suffix = suffix
-  let Prefix = prefix
+export const IconButton = forwardRef(
+  (
+    {
+      variant,
+      onClick,
+      href,
+      type,
+      disabled,
+      sharpLeft = false,
+      sharpRight = false,
+      className,
+      noRounded,
+      noRing,
+      icon,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <ButtonBase
+        {...props}
+        ref={ref}
+        iconOnly
+        label=""
+        variant={variant}
+        onClick={onClick}
+        href={href}
+        type={type}
+        disabled={disabled}
+        sharpLeft={sharpLeft}
+        sharpRight={sharpRight}
+        noRing={noRing}
+        noRounded={noRounded}
+        prefix={icon}
+        className={className}
+      />
+    );
+  }
+);
 
-  return (
-    <Component
-      initial={{ scale: 1 }}
-      whileTap={{ scale: 0.99 }}
-      ref={ref}
-      type={type}
-      className={cn(
-        {
-          "w-full": block,
-          "w-fit": !block
-        },
-        {
-          "bodyMd-medium": variant !== "primary-plain" && variant !== "secondary-plain" && variant !== "critical-plain" && variant !== "plain",
-          "bodyMd": variant === "primary-plain" || variant === "secondary-plain" || variant !== "critical-plain" || variant !== "plain",
-        },
-        "relative ring-offset-1",
-        "outline-none shadow-button",
-        "flex flex-row gap-lg items-center justify-center",
-        "disabled:text-text-disabled",
-        {
-          ...(noRing ? {} : {
-            "focus-visible:ring-2 focus:ring-border-focus focus:z-10": true
-          })
-        },
-        {
-          ...(noRounded ? {} : {
-            "rounded-none": sharpLeft && sharpRight,
-            "rounded-r": sharpLeft && !sharpRight,
-            "rounded-l": !sharpLeft && sharpRight,
-            "rounded": !sharpLeft && !sharpRight,
-          })
-        },
-        "transition-all",
-        "disabled:pointer-events-none",
-        {
-          ...(noBorder ? { "border-none": true } : {
-            "border-border-default disabled:border-border-disabled": variant === "basic" || variant === "outline",
-            "border-border-primary disabled:border-border-disabled": variant === "primary" || variant === "primary-outline",
-            "border-border-secondary disabled:border-border-disabled": variant === "secondary" || variant === "secondary-outline",
-            "border-border-critical disabled:border-border-disabled": variant === "critical-outline" || variant === "critical",
-            "border-none": variant === "plain" || variant === "primary-plain" || variant === "critical-plain" || variant === "secondary-plain",
-            "border": !(variant === "plain" || variant === "primary-plain" || variant === "critical-plain" || variant === "secondary-plain"),
-          })
-        },
-        {
-          "bg-surface-basic-default hover:bg-surface-basic-hovered active:bg-surface-basic-pressed disabled:bg-surface-basic-default": variant === "basic",
-          "bg-surface-basic-pressed hover:bg-surface-basic-pressed active:bg-surface-basic-pressed disabled:bg-surface-basic-default": variant === "basic" && selected,
-          "bg-surface-primary-default hover:bg-surface-primary-hovered active:bg-surface-primary-pressed disabled:bg-surface-basic-default": variant === "primary",
-          "bg-surface-secondary-default hover:bg-surface-secondary-hovered active:bg-surface-secondary-pressed disabled:bg-surface-basic-default": variant === "secondary",
-          "bg-surface-critical-default hover:bg-surface-critical-hovered active:bg-surface-critical-pressed disabled:bg-surface-basic-default": variant === "critical",
-          "bg-none hover:bg-surface-critical-subdued active:bg-surface-critical-subdued": variant === "critical-outline",
-          "bg-none hover:bg-surface-primary-subdued active:bg-surface-primary-subdued": variant === "primary-outline",
-          "bg-none hover:bg-surface-secondary-subdued active:bg-surface-secondary-subdued": variant === "secondary-outline",
-          "bg-none hover:bg-surface-basic-hovered active:bg-surface-basic-pressed": variant === "outline",
-          "bg-surface-basic-pressed shadow-none hover:bg-surface-basic-hovered active:bg-surface-basic-pressed hover:shadow-button active:shadow-button": variant === "outline" && selected,
-          "bg-none shadow-none active:bg-surface-basic-pressed active:shadow-button": variant === "plain" && !iconOnly,
-          "bg-none shadow-none active:bg-surface-primary-pressed active:shadow-button": variant === "primary-plain",
-          "bg-none shadow-none active:bg-surface-secondary-pressed active:shadow-button": variant === "secondary-plain",
-          "bg-none shadow-none active:bg-surface-critical-pressed active:shadow-button": variant === "critical-plain",
-          "bg-none shadow-none hover:bg-surface-basic-hovered active:bg-surface-basic-pressed active:shadow-button": variant === "plain" && iconOnly,
-        },
-        {
-          "text-text-default": (variant === "basic" || variant === "plain" || variant === "outline"),
-          "active:text-text-on-primary": (variant === "primary-plain" || variant === "critical-plain" || variant === "secondary-plain"),
-          "text-text-on-primary": variant === "primary" || variant === "critical" || variant === "secondary" || variant === "secondary-outline",
-          "text-text-critical hover": (variant === "critical-outline" || variant === "critical-plain"),
-          "text-text-primary": (variant === "primary-outline" || variant === "primary-plain"),
-          "text-text-secondary": variant === "secondary-plain" || variant === "secondary-outline",
-        },
-        {
-          "focus:underline": noRing
-        },
-        {
-          "hover:underline": variant === "plain" || variant === "primary-plain" || variant === "critical-plain" || variant === "secondary-plain",
-        },
-        {
-          "px-2xl py-lg": !iconOnly && !(variant === "plain" || variant === "primary-plain" || variant === "critical-plain" || variant === "secondary-plain"),
-          "px-md py-sm": !iconOnly && (variant === "plain" || variant === "primary-plain" || variant === "critical-plain" || variant === "secondary-plain"),
-          "p-lg": iconOnly
-        },
-        className
-      )}
-      {...props}
-      {...extraProps}
-    >
-      {prefix && <Prefix size={iconOnly ? 20 : 16} color="currentColor" />}
-      {!iconOnly && content}
-      {suffix && !iconOnly && <Suffix size={16} color="currentColor" />}
-    </Component>
-  );
-})
-
-
-export const IconButton = forwardRef(({
-  variant,
-  onClick,
-  href,
-  type,
-  disabled,
-  sharpLeft = false,
-  sharpRight = false,
-  className,
-  noRounded,
-  noRing,
-  icon,
-  ...props
-}, ref) => {
-
-  return <ButtonBase {...props} ref={ref} iconOnly={true} label={''} variant={variant} onClick={onClick} href={href} type={type} disabled={disabled} sharpLeft={sharpLeft} sharpRight={sharpRight} noRing={noRing} noRounded={noRounded} prefix={icon} className={className} />
-})
-
-
-
-export const Button = forwardRef(({
-  content,
-  variant,
-  onClick,
-  href,
-  type,
-  disabled,
-  sharpLeft = false,
-  sharpRight = false,
-  className,
-  noRounded,
-  noBorder,
-  noRing,
-  prefix,
-  suffix,
-  block,
-  LinkComponent,
-  ...props
-}, ref) => {
-  return <ButtonBase {...props} ref={ref} LinkComponent={LinkComponent} iconOnly={false} block={block} content={content} noBorder={noBorder} suffix={suffix} variant={variant} onClick={onClick} href={href} type={type} disabled={disabled} sharpLeft={sharpLeft} sharpRight={sharpRight} noRing={noRing} noRounded={noRounded} prefix={prefix} className={className} />
-})
+export const Button = forwardRef(
+  (
+    {
+      content,
+      variant,
+      onClick,
+      href,
+      type,
+      disabled,
+      sharpLeft = false,
+      sharpRight = false,
+      className,
+      noRounded,
+      noBorder,
+      noRing,
+      prefix,
+      suffix,
+      block,
+      LinkComponent,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <ButtonBase
+        {...props}
+        ref={ref}
+        LinkComponent={LinkComponent}
+        iconOnly={false}
+        block={block}
+        content={content}
+        noBorder={noBorder}
+        suffix={suffix}
+        variant={variant}
+        onClick={onClick}
+        href={href}
+        type={type}
+        disabled={disabled}
+        sharpLeft={sharpLeft}
+        sharpRight={sharpRight}
+        noRing={noRing}
+        noRounded={noRounded}
+        prefix={prefix}
+        className={className}
+      />
+    );
+  }
+);
 
 Button.propTypes = {
   variant: PropTypes.oneOf([
@@ -217,17 +334,16 @@ Button.propTypes = {
   onClick: PropTypes.func,
   href: PropTypes.string,
   disabled: PropTypes.bool,
-  type: PropTypes.oneOf(["button", "submit"]),
+  type: PropTypes.oneOf(['button', 'submit']),
   LinkComponent: PropTypes.any,
-}
+};
 
 Button.defaultProps = {
   variant: 'primary',
   onClick: undefined,
-  type: "button",
-  content: "button"
+  type: 'button',
+  content: 'button',
 };
-
 
 IconButton.propTypes = {
   /**
@@ -249,11 +365,11 @@ IconButton.propTypes = {
    * Disable button
    */
   disabled: PropTypes.bool,
-  type: PropTypes.oneOf(["button", "submit"]),
+  type: PropTypes.oneOf(['button', 'submit']),
 };
 
 IconButton.defaultProps = {
   variant: 'basic',
   onClick: undefined,
-  type: "button",
+  type: 'button',
 };
