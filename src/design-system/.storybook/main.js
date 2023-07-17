ab326122e6f13bdd18cc188f34f600f29ea03d7d
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-07-17 15:57:26 +0530

    :bug: Storybook vite configuration fix for the eslint and the compilation, alias is setted.

--- src/design-system/.storybook/main.js
+++ src/design-system/.storybook/main.js
@@ -1,4 +1,9 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
+import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { mergeConfig } from "vite"
+
 const config = {
   stories: [
     "../book/**/*.@(js|jsx|ts|tsx|mdx)",
@@ -6,7 +11,19 @@ const config = {
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {
+      builder: {
+      },
+    },
+  },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          "~/components": path.resolve(__dirname, '../components'),
+        },
+      },
+    })
   },
   docs: {
     autodocs: "tag",
