abedd58cff1bfc74c8df67f907e346713320d337
Bikash <31549600+tulsiojha@users.noreply.github.com>
2023-07-21 14:25:53 +0530

    Features/console design (#4)
    * Added max-width for component and console pages.
    
    * Added progress-bar component and useScreen hook
    
    ---------
    
    Co-authored-by: Bikash Ojha <tulsiojhaamg@gmail.com>

---
+++ src/design-system/components/hooks/useScreen.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+const useScreenSize = () => {
+  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+
+  function onResize(event) {
+    setScreenSize({
+      width: event.target.innerWidth,
+      height: event.target.innerHeight,
+    });
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (screenSize.width === 0 && screenSize.height === 0) {
+        onResize({ target: window });
+      }
+      window.addEventListener('resize', onResize, true);
+      return () => {
+        window.removeEventListener('resize', onResize);
+      };
+    }
+    return () => {};
+  }, []);
+
+  return screenSize;
+};
+
+export default useScreenSize;
