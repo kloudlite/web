9c8baefa9f58a52d3b986f830b284546795559a5
Bikash <31549600+tulsiojha@users.noreply.github.com>
2023-07-24 12:32:50 +0530

    Features/console design (#6)
    * Added max-width for component and console-pages.
    
    * Added progress-bar component and useScreen hook
    
    * Added ToastProvider in root for test purpose
    
    * Added Toast Component, Added spacing tokens
    
    ---------
    
    Co-authored-by: Bikash Ojha <tulsiojhaamg@gmail.com>

--- src/design-system/components/molecule/toast.jsx
+++ src/design-system/components/molecule/toast.jsx
@@ -1,39 +1,105 @@
-import { useState } from 'react';
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useReducer,
+  useState,
+} from 'react';
 import * as ToastRadix from '@radix-ui/react-toast';
-import { Button } from '../atoms/button';
+import { v4 as uuid } from 'uuid';
+import { X } from '@jengaicons/react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Button, IconButton } from '../atoms/button';
 import { cn } from '../utils';
 
-export const ToastProvider = ({ children, duration = 500 }) => {
+const context = createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return [...state, action.data];
+    default:
+      return state;
+  }
+};
+
+const AnimatedToast = ({ toast }) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          layoutId={toast.id}
+          initial={{ y: 3 }}
+          animate={{ y: 0 }}
+          exit={{ y: 3 }}
+        >
+          <ToastRadix.Root
+            forceMount
+            className={cn(
+              'toast flex flex-row bg-surface-tertiary-default border-border-tertiary rounded shadow-popover text-text-on-primary p-xl gap-xl items-center justify-between'
+            )}
+          >
+            <ToastRadix.Title className="bodyMd-medium truncate">
+              {toast.content}
+            </ToastRadix.Title>
+            {/* <ToastRadix.Action asChild altText="undo">
+              <Button
+                content={<span className="text-text-on-primary">Undo</span>}
+                variant="plain"
+                className="text-text-on-primary"
+              />
+            </ToastRadix.Action> */}
+            <ToastRadix.Action asChild altText="close">
+              <button>
+                <X size={12} color="currentColor" />
+              </button>
+            </ToastRadix.Action>
+          </ToastRadix.Root>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export const ToastProvider = ({ duration, children }) => {
+  const [toasts, dispatch] = useReducer(reducer, []);
+  const [open, setOpen] = useState(false);
+
   return (
     <ToastRadix.Provider swipeDirection="right" duration={duration}>
-      {children}
-      <ToastRadix.Viewport className="flex flex-col gap-2 fixed bottom-2 right-2 m-0 list-none z-[2147483647] outline-none" />
+      <context.Provider
+        value={useMemo(
+          () => ({
+            toasts,
+            dispatch,
+          }),
+          [toasts, dispatch]
+        )}
+      >
+        {toasts.map((toast) => (
+          <AnimatedToast toast={toast} key={toast.id} />
+        ))}
+        {children}
+      </context.Provider>
+      <ToastRadix.Viewport className="flex flex-col gap-lg fixed w-13xl bottom-8xl left-11xl m-0 list-none z-[2147483647] outline-none" />
     </ToastRadix.Provider>
   );
 };
 
-export const Toast = ({ show }) => {
-  const [open, setOpen] = useState(show);
-  return (
-    <ToastRadix.Root
-      className={cn(
-        'toast flex flex-row bg-surface-tertiary-default border-border-tertiary rounded shadow-popover text-text-on-primary p-3 gap-3 items-center justify-between',
-        'data-[state=open]:animate-slideIn data-[state=closed]:animate-slideOut data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut'
-      )}
-      open={open}
-      onOpenChange={setOpen}
-    >
-      <ToastRadix.Title className="bodyMd-medium">
-        Scheduled: Catch up
-      </ToastRadix.Title>
-      <ToastRadix.Action asChild altText="undo">
-        <Button
-          content="Undo"
-          variant="secondary-plain"
-          size="small"
-          className="text-text-surface-primary"
-        />
-      </ToastRadix.Action>
-    </ToastRadix.Root>
-  );
+export const useToast = () => {
+  const { toasts, dispatch } = useContext(context);
+
+  const addContent = (content) => {
+    if (content) {
+      dispatch({ type: 'ADD', data: { content, id: uuid() } });
+      console.log(toasts);
+    }
+  };
+  return {
+    toast: async ({ content }) => {
+      addContent(content);
+    },
+  };
 };
