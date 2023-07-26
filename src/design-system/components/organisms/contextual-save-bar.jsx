3a363ab44725349a3a3b1847c3deac72ac9099ea
Bikash Ojha <tulsiojhaamg@gmail.com>
2023-07-26 11:16:36 +0530

    Wrapped contextual-save-bar and navbar  with container component to make it responsive

--- src/design-system/components/organisms/contextual-save-bar.jsx
+++ src/design-system/components/organisms/contextual-save-bar.jsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import { Button } from '../atoms/button';
 import { BrandLogo } from '../branding/brand-logo';
+import Container from '../atoms/container';
 
 export const ContextualSaveBar = ({
   logo,
@@ -13,46 +14,48 @@ export const ContextualSaveBar = ({
   return (
     <div
       className={classNames(
-        'transition-all bg-surface-secondary-pressed py-xl px-3xl md:px-0',
+        'transition-all bg-surface-secondary-pressed py-xl',
         {
           'sticky top-0 left-0 right-0 z-40': fixed,
         }
       )}
     >
-      <div className="flex flex-row items-center justify-between m-auto max-w-8xl gap-lg md:gap-0">
-        {logo && (
-          <div
-            className="hidden md:block lg:block xl:block"
-            width={logoWidth || 124}
-          >
-            {logo}
-          </div>
-        )}
-        {message && (
-          <div className="headingMd text-text-on-primary font-sans-serif truncate">
-            {message}
-          </div>
-        )}
-        {logo && (
-          <>
-            <div className="hidden md:block" />
-            <div className="hidden md:block" />
-            <div className="hidden md:block" />
-          </>
-        )}
-        <div className="gap-x-lg flex flex-row items-center">
-          {discardAction && (
-            <Button
-              content="Discard"
-              onClick={discardAction}
-              variant="secondary-outline"
-            />
+      <Container>
+        <div className="flex flex-row items-center justify-between gap-lg md:gap-0">
+          {logo && (
+            <div
+              className="hidden md:block lg:block xl:block"
+              width={logoWidth || 124}
+            >
+              {logo}
+            </div>
           )}
-          {saveAction && (
-            <Button content="Publish" onClick={saveAction} variant="basic" />
+          {message && (
+            <div className="headingMd text-text-on-primary font-sans-serif truncate">
+              {message}
+            </div>
           )}
+          {logo && (
+            <>
+              <div className="hidden md:block" />
+              <div className="hidden md:block" />
+              <div className="hidden md:block" />
+            </>
+          )}
+          <div className="gap-x-lg flex flex-row items-center">
+            {discardAction && (
+              <Button
+                content="Discard"
+                onClick={discardAction}
+                variant="secondary-outline"
+              />
+            )}
+            {saveAction && (
+              <Button content="Publish" onClick={saveAction} variant="basic" />
+            )}
+          </div>
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
