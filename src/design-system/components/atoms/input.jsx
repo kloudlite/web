e20eef0585eb500822ba25dfb0da468aa791d0dd
Bikash Ojha <tulsiojhaamg@gmail.com>
2023-08-09 11:09:58 +0530

    Add cluster, vpn, domain pages

--- src/design-system/components/atoms/input.jsx
+++ src/design-system/components/atoms/input.jsx
@@ -186,6 +186,8 @@ export const TextInputBase = forwardRef((props, ref) => {
 
   const Component = component || 'input';
 
+  console.log(extraProps);
+
   return (
     <div
       className={cn('flex flex-col ', {
