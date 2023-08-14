50e41611a96e7f0fe129754c47ded9884f20d370
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-08-14 18:46:05 +0530

    :arrow_up: Changes done according to api changes.

--- src/design-system/components/atoms/container.jsx
+++ src/design-system/components/atoms/container.jsx
@@ -1,8 +1,14 @@
 const Container = ({ children }) => {
   return (
-    <div className="w-full max-w-8xl mx-auto px-3xl md:px-6xl lg:px-9xl xl:px-11xl 2xl:px-lg">
-      {children}
-    </div>
+    <>
+      <div className="flex justify-center px-3xl">
+        <div className="flex-1 w-full max-w-8xl min-w-[320px]">{children}</div>
+      </div>
+
+      {/* <div className="w-full max-w-8xl mx-auto px-3xl md:px-6xl lg:px-9xl xl:px-11xl 2xl:px-lg"> */}
+      {/*   {children} */}
+      {/* </div> */}
+    </>
   );
 };
 
