9cda57ac1aefb374ea9e36201f3cf507fcfd5fcc
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-08-21 18:36:15 +0530

    :art: Navbar and topbar updated for the screens

--- src/design-system/components/organisms/top-bar.jsx
+++ src/design-system/components/organisms/top-bar.jsx
@@ -1,24 +1,7 @@
-import { ChevronLeft } from '@jengaicons/react';
 import Container from '../atoms/container';
-import Tabs from '../atoms/tabs';
 import { cn } from '../utils';
-import ScrollArea from '../atoms/scroll-area';
 
-export const TopBar = ({
-  tab,
-  actions,
-  logo,
-  fixed,
-  linkComponent,
-  breadcrum = null,
-  backurl = {},
-}) => {
-  let BackurlComp = linkComponent;
-  let backurlProps = { to: backurl?.href };
-  if (!BackurlComp) {
-    BackurlComp = 'a';
-    backurlProps = { href: backurl?.href };
-  }
+export const TopBar = ({ tabs, actions, logo, fixed, breadcrum = null }) => {
   return (
     <div
       className={cn(
@@ -40,42 +23,7 @@ export const TopBar = ({
             </div>
           </div>
         </div>
-        {(!!tab?.items || !!tab?.backurl) && (
-          <div className="flex flex-row items-center">
-            {backurl && (
-              <div className="flex flex-row items-center">
-                <BackurlComp
-                  {...backurlProps}
-                  className="outline-none flex flex-row items-center gap-lg bodyMd-medium text-text-soft hover:text-text-default active:text-text-default py-lg cursor-pointer"
-                >
-                  <ChevronLeft size={16} />
-                  {backurl.name}
-                </BackurlComp>
-                <span className="ml-4xl mr-2xl w-xs h-2xl bg-border-default" />
-              </div>
-            )}
-            {/* <div className="-mx-3xl md:mx-0">
-             
-            </div> */}
-            <ScrollArea
-              blurfrom="from-white"
-              rightblur={false}
-              className="flex-1 -mr-2xl"
-            >
-              <Tabs.Root
-                basePath={tab.basePath}
-                value={tab.value}
-                fitted={tab.fitted}
-                onChange={tab.onChange}
-                LinkComponent={linkComponent}
-              >
-                {tab.items?.map((tabitem, index) => {
-                  return <Tabs.Tab {...tabitem} key={index} />;
-                })}
-              </Tabs.Root>
-            </ScrollArea>
-          </div>
-        )}
+        {!!tabs && tabs}
       </Container>
     </div>
   );
