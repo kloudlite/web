e58c30918bfd9d57ae13276a98c4f17569cd01f3
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-09-05 11:00:07 +0530

    :art: Progress tracker Using structure updated.

--- src/design-system/components/utils.tsx
+++ src/design-system/components/utils.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { useMemo } from 'react';
 
 export { v4 as uuid } from 'uuid';
 
@@ -9,3 +10,12 @@ export const cn = (...props: cnProps) => {
 };
 
 export const _false = false;
+
+export function mapper<A, B>(array: A[], transform: (value: A) => B): B[] {
+  let _;
+  return array.map(transform);
+}
+
+export function useMapper<A, B>(array: A[], transform: (value: A) => B): B[] {
+  return useMemo(() => array.map(transform), [array]);
+}
