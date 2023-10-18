f10ed49632423b0f44959e8acafaf79705eccc8a
Bikash Ojha <tulsiojhaamg@gmail.com>
2023-10-18 19:08:22 +0530

    Added new typography

--- src/design-system/css-plugins/typography.js
+++ src/design-system/css-plugins/typography.js
@@ -1,8 +1,12 @@
 function typography() {
+  // @ts-ignore
   return ({ addComponents }) => {
     addComponents({
       '.bodyMono': {
         '@apply text-sm leading-sm font-mono': {},
+      },
+      '.bodyMono-medium': {
+        '@apply bodyMono font-medium': {},
       },
       '.bodySm': {
         '@apply text-xs leading-xs': {},
@@ -43,20 +47,41 @@ function typography() {
       '.headingMd': {
         '@apply font-semibold text-md leading-md': {},
       },
+      '.headingMd-marketing': {
+        '@apply headingMd font-familjen': {},
+      },
       '.headingLg': {
         '@apply font-semibold text-lg leading-md': {},
+      },
+      '.headingLg-marketing': {
+        '@apply headingLg font-familjen': {},
       },
       '.headingXl': {
         '@apply font-semibold text-xl leading-lg': {},
       },
+      '.headingXl-marketing': {
+        '@apply headingXl font-familjen': {},
+      },
       '.heading2xl': {
         '@apply font-semibold text-2xl leading-xl': {},
+      },
+      '.heading2xl-marketing': {
+        '@apply heading2xl font-familjen': {},
       },
       '.heading3xl': {
         '@apply font-semibold text-3xl leading-2xl': {},
       },
+      '.heading3xl-marketing': {
+        '@apply heading3xl font-familjen': {},
+      },
       '.heading4xl': {
         '@apply font-bold text-4xl leading-3xl': {},
+      },
+      '.heading4xl-marketing': {
+        '@apply heading4xl font-familjen': {},
+      },
+      '.heading5xl-marketing': {
+        '@apply font-bold text-6xl leading-6xl font-familjen': {},
       },
     });
   };
