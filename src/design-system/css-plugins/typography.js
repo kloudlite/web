9642cde72d53747690c9c184567884ce5384d4f4
Bikash <bikash@Bikashs-MacBook-Air.local>
2023-11-09 11:01:35 +0530

    fixed iconbutton icon size and added font-sans as default font

--- src/design-system/css-plugins/typography.js
+++ src/design-system/css-plugins/typography.js
@@ -9,7 +9,7 @@ function typography() {
         '@apply bodyMono font-medium': {},
       },
       '.bodySm': {
-        '@apply text-xs leading-xs': {},
+        '@apply text-xs leading-xs font-sans': {},
       },
       '.bodySm-medium': {
         '@apply bodySm font-medium': {},
@@ -18,7 +18,7 @@ function typography() {
         '@apply bodySm font-semibold': {},
       },
       '.bodyMd': {
-        '@apply text-sm leading-sm': {},
+        '@apply text-sm leading-sm font-sans': {},
       },
       '.bodyMd-medium': {
         '@apply bodyMd font-medium': {},
@@ -30,7 +30,7 @@ function typography() {
         '@apply bodyMd underline': {},
       },
       '.bodyLg': {
-        '@apply text-md leading-md': {},
+        '@apply text-md leading-md font-sans': {},
       },
       '.bodyLg-medium': {
         '@apply bodyLg font-medium': {},
@@ -39,43 +39,43 @@ function typography() {
         '@apply bodyLg underline': {},
       },
       '.headingXs': {
-        '@apply font-semibold text-xs leading-xs': {},
+        '@apply font-semibold text-xs leading-xs font-sans': {},
       },
       '.headingSm': {
-        '@apply font-semibold text-sm leading-sm': {},
+        '@apply font-semibold text-sm leading-sm font-sans': {},
       },
       '.headingMd': {
-        '@apply font-semibold text-md leading-md': {},
+        '@apply font-semibold text-md leading-md font-sans': {},
       },
       '.headingMd-marketing': {
         '@apply headingMd font-familjen': {},
       },
       '.headingLg': {
-        '@apply font-semibold text-lg leading-md': {},
+        '@apply font-semibold text-lg leading-md font-sans': {},
       },
       '.headingLg-marketing': {
         '@apply headingLg font-familjen': {},
       },
       '.headingXl': {
-        '@apply font-semibold text-xl leading-lg': {},
+        '@apply font-semibold text-xl leading-lg font-sans': {},
       },
       '.headingXl-marketing': {
         '@apply headingXl font-familjen': {},
       },
       '.heading2xl': {
-        '@apply font-semibold text-2xl leading-xl': {},
+        '@apply font-semibold text-2xl leading-xl font-sans': {},
       },
       '.heading2xl-marketing': {
         '@apply heading2xl font-familjen': {},
       },
       '.heading3xl': {
-        '@apply font-semibold text-3xl leading-2xl': {},
+        '@apply font-semibold text-3xl leading-2xl font-sans': {},
       },
       '.heading3xl-marketing': {
         '@apply heading3xl font-familjen': {},
       },
       '.heading4xl': {
-        '@apply font-bold text-4xl leading-3xl': {},
+        '@apply font-bold text-4xl leading-3xl font-sans': {},
       },
       '.heading4xl-marketing': {
         '@apply heading4xl font-familjen': {},
