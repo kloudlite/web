9a7084838827d58e08e61dce28ff4a17d4b1b0d9
Bikash <tulsiojhaamg@gmail.com>
2024-01-03 15:50:35 +0530

    WIP - warning - do not revert to this commit

---
+++ src/design-system/storybook-dist/storybook/assets/IconButton.stories-a790cd98.js
@@ -0,0 +1,6 @@
+import{j as s}from"./jsx-runtime-29545a09.js";import{I as n}from"./button-30fb7d7d.js";import{C as e}from"./CalendarCheckFill-682d21c0.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./utils-e72801fa.js";import"./index-e131923d.js";import"./motion-e61dcc0d.js";import"./index-add589df.js";import"./use-force-update-d6d40804.js";import"./Spinner-6e10423b.js";import"./context-494caab9.js";const x={title:"Atoms/IconButton",component:n,tags:["autodocs"],argTypes:{}},o={args:{variant:"basic",icon:s(e,{})}};var t,r,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    variant: 'basic',
+    icon: <CalendarCheckFill />
+  }
+}`,...(a=(r=o.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const _=["BaseButton"];export{o as BaseButton,_ as __namedExportsOrder,x as default};
