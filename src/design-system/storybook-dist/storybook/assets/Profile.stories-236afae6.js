59bb2c468123dedf61f66e6d76d8dc73019eebab
Bikash <tulsiojhaamg@gmail.com>
2024-01-03 15:50:35 +0530

    WIP - warning - do not revert to this commit

---
+++ src/design-system/storybook-dist/storybook/assets/Profile.stories-236afae6.js
@@ -0,0 +1,7 @@
+import{P as s}from"./profile-16b174b1.js";import"./jsx-runtime-29545a09.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./avatar-7a638a71.js";import"./utils-e72801fa.js";import"./index-e131923d.js";import"./bounce-it-356267b3.js";import"./motion-e61dcc0d.js";const f={title:"Molecules/Profile",component:s,tags:["autodocs"],argTypes:{}},r={args:{name:"Karthik Th",subtitle:"subtitle",color:"one"}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    name: 'Karthik Th',
+    subtitle: 'subtitle',
+    color: 'one'
+  }
+}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const P=["ProfileAvatar"];export{r as ProfileAvatar,P as __namedExportsOrder,f as default};
