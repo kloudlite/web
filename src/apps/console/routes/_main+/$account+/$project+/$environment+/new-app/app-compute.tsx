bcc481a8816e831c91b9293fb6d4a61610171044
nxtcoder36 <mohit0698@gmail.com>
2024-03-15 15:14:21 +0530

    label changed for image pull policy in app creation and app settings page

--- src/apps/console/routes/_main+/$account+/$project+/$environment+/new-app/app-compute.tsx
+++ src/apps/console/routes/_main+/$account+/$project+/$environment+/new-app/app-compute.tsx
@@ -320,7 +320,7 @@ const AppCompute = () => {
         />
 
         <Checkbox
-          label="Image Pull Policy"
+          label="Always pull image on restart"
           checked={values.imagePullPolicy === 'Always'}
           onChange={(val) => {
             const imagePullPolicy = val ? 'Always' : 'IfNotPresent';
