d3a35798ead69b42f3ce7858d90eb6ec073739c7
Abdhesh Nayak <abdheshnayak@gmail.com>
2024-05-01 12:43:11 +0530

    :art: UI improvements

--- src/design-system/components/atoms/container.tsx
+++ src/design-system/components/atoms/container.tsx
@@ -9,9 +9,7 @@ interface IContainer {
 const Container = ({ children, className = '' }: IContainer) => {
   return (
     <div className={cn('flex flex-1 justify-center px-8xl', className)}>
-      <div className="flex-1 w-full  min-w-[320px] max-w-screen-2xl">
-        {children}
-      </div>
+      <div className="flex-1 w-full  min-w-[320px] max-w-8xl">{children}</div>
     </div>
   );
 };
