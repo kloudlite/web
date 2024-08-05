27de51c9f0d12d3118fe4436ab40f18384064854
Bikash <tulsiojhaamg@gmail.com>
2024-08-05 16:48:10 +0545

    fixed ts error

--- src/apps/devdoc/pages/404.tsx
+++ src/apps/devdoc/pages/404.tsx
@@ -1,4 +1,5 @@
 import Page404 from 'kl-design-system/organisms/page-404';
+
 export default function fourOfour() {
   return <Page404 link="/" />;
 }
--- src/apps/devdoc/pages/500.tsx
+++ src/apps/devdoc/pages/500.tsx
@@ -1,4 +1,5 @@
 import Page500 from 'kl-design-system/organisms/page-500';
-export default function fourOfour() {
+
+export default function fiveOO() {
   return <Page500 link="/" />;
 }
