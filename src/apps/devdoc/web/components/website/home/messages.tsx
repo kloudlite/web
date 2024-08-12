8391b483da08048b71e71d8ce4a6f84c325b9644
Bikash <tulsiojhaamg@gmail.com>
2024-08-12 11:07:22 +0545

    fixed messages font sizes

--- src/apps/devdoc/web/components/website/home/messages.tsx
+++ src/apps/devdoc/web/components/website/home/messages.tsx
@@ -7,6 +7,54 @@ import consts from '~/app/utils/const';
 import { Block } from '../../commons';
 import { GraphItem } from '../../graph';
 import Slider from '../../slider';
+import { IAvatar } from 'kl-design-system/atoms/avatar';
+
+const TitleItem = ({ title }: { title: ReactNode }) => {
+  return (
+    <span className="wb-headingLg-marketing wb-headingXl-marketing">
+      {title}
+    </span>
+  );
+};
+
+const SubTitleItem = ({ title }: { title: ReactNode }) => {
+  return <span className="wb-bodyLg">{title}</span>;
+};
+
+const ProfileItem = ({
+  title,
+  subtitle,
+  size = 'md',
+}: {
+  title?: ReactNode;
+  subtitle?: ReactNode;
+  size?: IAvatar['size'];
+}) => {
+  return (
+    <div>
+      <span className="wb-flex-1 dark-hidden">
+        <Profile
+          responsive={false}
+          name={<TitleItem title={title} />}
+          subtitle={<SubTitleItem title={subtitle} />}
+          color="one"
+          size={size}
+          tabIndex={-1}
+        />
+      </span>
+      <span className="wb-flex-1 wb-hidden dark-block">
+        <Profile
+          responsive={false}
+          name={<TitleItem title={title} />}
+          subtitle={<SubTitleItem title={subtitle} />}
+          color="dark"
+          size={size}
+          tabIndex={-1}
+        />
+      </span>
+    </div>
+  );
+};
 
 const MessageCard = ({
   title,
@@ -18,30 +66,18 @@ const MessageCard = ({
   message: ReactNode;
 }) => {
   return (
-    <div className="xl:wb-min-h-[320px] 2xl:wb-max-h-[288px] 3xl:wb-min-h-[256px] wb-flex wb-flex-col wb-gap-3xl wb-p-3xl wb-h-full wb-bg-surface-basic-default wb-min-h-[224px]">
-      <div className="wb-flex wb-flex-row wb-items-center wb-gap-3xl">
-        <span className="wb-flex-1 dark-hidden">
-          <Profile
-            responsive={false}
-            name={<span className="wb-bodyLg-medium">{title}</span>}
-            subtitle={<span className="wb-bodyMd">{subtitle}</span>}
-            color="one"
-            size="md"
-            tabIndex={-1}
-          />
-        </span>
-        <span className="wb-flex-1 wb-hidden dark-block">
-          <Profile
-            responsive={false}
-            name={<span className="wb-bodyLg-medium">{title}</span>}
-            subtitle={<span className="wb-bodyMd">{subtitle}</span>}
-            color="dark"
-            size="md"
-            tabIndex={-1}
-          />
-        </span>
+    <div className="xl:wb-min-h-[480px] 2xl:wb-max-h-[416px] 3xl:wb-min-h-[352px] wb-flex wb-flex-col wb-gap-3xl wb-p-3xl wb-h-full wb-bg-surface-basic-default wb-min-h-[224px]">
+      <div className="wb-flex md:wb-hidden xl:wb-flex wb-flex-row wb-items-center wb-gap-3xl">
+        <ProfileItem subtitle={subtitle} title={title} />
       </div>
-      <p className="wb-bodyLg wb-text-text-soft">{message}</p>
+      <div className="wb-hidden md:wb-flex xl:wb-hidden wb-flex-col wb-gap-3xl">
+        <ProfileItem size="lg" />
+        <div className="wb-flex wb-flex-col wb-gap-lg">
+          <TitleItem title={title} />
+          <SubTitleItem title={subtitle} />
+        </div>
+      </div>
+      <p className="wb-bodyLg lg:wb-bodyXl wb-text-text-soft">{message}</p>
     </div>
   );
 };
