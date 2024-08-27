b8974c18f98ebd538e5efac2b513927fb01fe6db
Bikash <tulsiojhaamg@gmail.com>
2024-08-27 12:35:48 +0545

    fixed zindex issue:

--- src/apps/devdoc/web/layout/theme.tsx
+++ src/apps/devdoc/web/layout/theme.tsx
@@ -201,7 +201,7 @@ const Main = ({ children, pageOpts }: NextraThemeLayoutProps) => {
           )}
           <article
             className={cn(
-              'wb-flex-1 wb-w-full z-50',
+              'wb-flex-1 wb-w-full md:!wb-z-50',
               activeThemeContext.layout === 'raw' ? '' : 'lg:wb-pt-xl',
             )}
           >
