66f39344ba76c8fa142beb79b3ddc5f4d47f7bea
Bikash <tulsiojhaamg@gmail.com>
2024-09-13 16:20:31 +0545

    wip

--- src/apps/devdoc/functions/src/index.ts
+++ src/apps/devdoc/functions/src/index.ts
@@ -8,30 +8,10 @@
  */
 
 import { onDocumentCreated } from 'firebase-functions/v2/firestore';
-import axios from 'axios';
 
 exports.contactEmail = onDocumentCreated(
   'web-contact-form/{formId}',
   async (event) => {
-    if (event.data) {
-      const newValue = event.data.data(); // The new document's data
-
-      // Access fields in the new document
-
-      console.log('hello New user created:', newValue);
-      const x = await axios('https://webhooks.dev.kloudlite.io/contact-us', {
-        method: 'post',
-        data: {
-          name: 'bikash ojha',
-          email: 'bikash@kloudlite.io',
-          mobileNo: '7004667341',
-          companyName: 'kl india',
-          country: 'india',
-          message: 'hello world',
-        },
-      });
-      console.log(x.data);
-    }
     return null;
   },
 );
